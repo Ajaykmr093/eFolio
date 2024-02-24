ea1e4e6476354dba34be99cf7cd9f20f83a09493
Ajaykmr093 <100403386+Ajaykmr093@users.noreply.github.com>
2024-02-24 14:32:00 +0000

    Add morgan for logging

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 import express from 'express';
+import morgan from 'morgan';
+
 const app = express();
 const port = 3000;
 
 app.use(express.static('public'));
+app.use(morgan('dev'));
 
 
 app.listen(port, () => {
