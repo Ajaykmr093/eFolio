715f382d5216d46dff911deab7ec0d432e774ca4
Ajaykmr093 <100403386+Ajaykmr093@users.noreply.github.com>
2024-02-24 14:37:29 +0000

    Add auth api using mock db

--- app.js
+++ app.js
@@ -1,11 +1,18 @@
 import express from 'express';
 import morgan from 'morgan';
+import bodyParser from 'body-parser';
+import authRoutes from './api/routes/auth.js';
 
 const app = express();
 const port = 3000;
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
 app.use(express.static('public'));
 app.use(morgan('dev'));
+
+app.use('/api/auth', authRoutes);
 
 
 app.listen(port, () => {
