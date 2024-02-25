b2d02237ca060f7c565b27ef59fb94da6d06b433
Ajaykmr093 <100403386+Ajaykmr093@users.noreply.github.com>
2024-02-25 14:25:12 +0000

    Use ejs, again

--- src/app.js
+++ src/app.js
@@ -14,6 +14,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(express.static("public"));
 app.use(morgan("dev"));
+app.set("view engine", "ejs");
 
 app.use(
   session({
--- src/routes/auth.js
+++ src/routes/auth.js
@@ -3,7 +3,13 @@ import { login, register } from "../controllers/authController.js";
 
 const router = express.Router();
 
-router.post("/login", login);
-router.post("/register", register);
+const loginRoute = router.route("/login");
+const registerRoute = router.route("/register");
+
+loginRoute.get((_, res) => res.render("pages/login"));
+registerRoute.get((_, res) => res.render("pages/register"));
+
+loginRoute.post(login);
+registerRoute.post(register);
 
 export default router;
