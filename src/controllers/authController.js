9ac3d476fa98e7dcac15ccf41476f7f16ad39483
Ajaykmr093 <100403386+Ajaykmr093@users.noreply.github.com>
2024-02-25 08:06:32 +0000

    Add index page

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,23 +1,26 @@
 import User from "../models/User.js";
 import { compare } from "bcrypt";
-import jwt from "jsonwebtoken";
 
 export async function register(req, res) {
   try {
     const { email, username, password } = req.body;
-    const user = new User({ email, username, password });
+    const user = new User({
+      email: email.toLowerCase(),
+      username: username.toLowerCase(),
+      password: password,
+    });
     await user.save();
     return res.status(200).render("pages/register", {
       info: "Registration successful. Please login.",
       warn: null,
-      error: null
+      error: null,
     });
   } catch (error) {
     if (error.code === 11000 || error.code === 11001) {
       return res.status(409).render("pages/register", {
         info: null,
         warn: "Email/Username already exists.",
-        error: null
+        error: null,
       });
     }
     console.error(error);
@@ -33,7 +36,10 @@ export async function login(req, res) {
   try {
     const { name, password } = req.body;
 
-    const user = await User.findOne().or([{ username: name }, { email: name }]);
+    const user = await User.findOne().or([
+      { username: name.toLowerCase() },
+      { email: name.toLowerCase() },
+    ]);
     if (!user) {
       return res.status(401).render("pages/login", {
         info: null,
@@ -51,13 +57,7 @@ export async function login(req, res) {
       });
     }
 
-    const token = jwt.sign(
-      { userId: user._id },
-      "App secret which should be in environment variable or something but I am lazy, so hard coding this.",
-      { expiresIn: "1h" }
-    );
-
-    req.session.token = token;
+    req.session.userId = user._id;
     return res.redirect("/");
   } catch (error) {
     console.error(error);
@@ -67,4 +67,9 @@ export async function login(req, res) {
       error: "Internal server error",
     });
   }
+}
+
+export async function getUserById(id) {
+  const user = await User.findOne({ _id: id });
+  return user;
 }
