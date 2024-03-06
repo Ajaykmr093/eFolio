36a0e2133fbc834c8432f40f855e8b05dc822e12
Vipul Tyagi <90324465+VipulOG@users.noreply.github.com>
2024-03-06 14:58:57 +0000

    Add home page

---
+++ migrate/src/routes/+page.svelte
@@ -0,0 +1,20 @@
+<script>
+	import logo from '$lib/assets/logo.png';
+</script>
+
+<div class="w-full h-full flex justify-center items-center">
+	<div class="text-center space-y-4">
+		<img src={logo} alt="Logo" class="mx-auto mb-4 w-32 h-32 filter drop-shadow-lg" />
+		<h1 class="text-2xl font-semibold mb-4">Carry Your Stories Everywhere with eFolio</h1>
+		<div class="flex flex-col md:flex-row gap-4 justify-center">
+			<a href="/" class="btn variant-filled">
+				<i class="fa-solid fa-user-plus" />
+				<span>Signup</span>
+			</a>
+			<a href="/" class="btn variant-filled">
+				<i class="fa-solid fa-right-to-bracket" />
+				<span>Login</span>
+			</a>
+		</div>
+	</div>
+</div>
