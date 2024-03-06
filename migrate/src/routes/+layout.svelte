278119692bd246e02deae5e1cf72592dd852a99c
Vipul Tyagi <90324465+VipulOG@users.noreply.github.com>
2024-03-06 13:41:30 +0000

    Update root page and layout

--- migrate/src/routes/+layout.svelte
+++ migrate/src/routes/+layout.svelte
@@ -1,8 +1,12 @@
+<!-- Layout: (root) -->
 <script lang="ts">
+	// Global Stylesheets
 	import '../app.postcss';
-	import { AppShell, AppBar } from '@skeletonlabs/skeleton';
 
-	// Floating UI for Popups
+	// Components & Utilities
+	import { AppShell, AppBar, LightSwitch } from '@skeletonlabs/skeleton';
+
+	// Dependency: Floating UI
 	import { computePosition, autoUpdate, flip, shift, offset, arrow } from '@floating-ui/dom';
 	import { storePopup } from '@skeletonlabs/skeleton';
 	storePopup.set({ computePosition, autoUpdate, flip, shift, offset, arrow });
@@ -10,40 +14,29 @@
 
 <!-- App Shell -->
 <AppShell>
+	<!-- Header -->
 	<svelte:fragment slot="header">
-		<!-- App Bar -->
 		<AppBar>
 			<svelte:fragment slot="lead">
-				<strong class="text-xl uppercase">Skeleton</strong>
+				<strong class="text-xl">eFolio</strong>
 			</svelte:fragment>
 			<svelte:fragment slot="trail">
-				<a
-					class="btn btn-sm variant-ghost-surface"
-					href="https://discord.gg/EXqV7W8MtY"
-					target="_blank"
-					rel="noreferrer"
-				>
-					Discord
-				</a>
-				<a
-					class="btn btn-sm variant-ghost-surface"
-					href="https://twitter.com/SkeletonUI"
-					target="_blank"
-					rel="noreferrer"
-				>
-					Twitter
-				</a>
-				<a
-					class="btn btn-sm variant-ghost-surface"
-					href="https://github.com/skeletonlabs/skeleton"
-					target="_blank"
-					rel="noreferrer"
-				>
-					GitHub
-				</a>
+				<LightSwitch />
 			</svelte:fragment>
 		</AppBar>
 	</svelte:fragment>
-	<!-- Page Route Content -->
+
+	<!-- Page Content -->
 	<slot />
+
+	<!-- Page Footer -->
+	<svelte:fragment slot="pageFooter">
+		<AppBar>
+			Built by
+			<a class="anchor" href="https://github.com/Ajaykmr093">Ajay</a>
+			and
+			<a class="anchor" href="https://github.com/VipulOG">Vipul</a>. The source code is available on
+			<a class="anchor" href="https://github.com/Ajaykmr093/library-management">GitHub</a>.
+		</AppBar>
+	</svelte:fragment>
 </AppShell>
