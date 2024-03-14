df3b08c30a727b97269704d1b5a2242d4e48aaf9
Vipul Tyagi <90324465+VipulOG@users.noreply.github.com>
2024-03-14 15:17:47 +0000

    Complete signin page with validation

---
+++ migrate/src/routes/auth/login/+page.server.ts
@@ -0,0 +1,42 @@
+import { redirect, type Actions, fail } from '@sveltejs/kit';
+import type { PageServerLoad } from './$types';
+import { db } from '$lib/server/surreal';
+import { message, superValidate } from 'sveltekit-superforms/server';
+import { zod } from 'sveltekit-superforms/adapters';
+import { schema } from './schema';
+
+export const load = (async () => {
+	const form = await superValidate(zod(schema));
+	return { form };
+}) satisfies PageServerLoad;
+
+export const actions = {
+	default: async ({ request, cookies }) => {
+		const form = await superValidate(request, zod(schema));
+		if (!form.valid) return fail(400, { form });
+
+		const { uid, password, remember } = form.data;
+
+		try {
+			const token = await db.signin({ scope: 'user', uid, password });
+			if (!token) message(form, 'Authentication failed.', { status: 401 });
+
+			const maxAge = remember ? 60 * 60 * 24 * 7 : undefined;
+			cookies.set('token', token, {
+				path: '/',
+				httpOnly: true,
+				sameSite: 'strict',
+				secure: true,
+				maxAge: maxAge,
+				priority: 'high'
+			});
+		} catch (err) {
+			if ((err as Error).message.includes('No record was returned')) {
+				return message(form, 'Invalid credentials.', { status: 401 });
+			}
+			return message(form, 'Somthing went wrong.', { status: 500 });
+		}
+
+		redirect(303, '/');
+	}
+} satisfies Actions;
