eccaf552af02697900bf8e36deb13d7e2dcd5adb
Vipul Tyagi <90324465+VipulOG@users.noreply.github.com>
2024-03-14 15:48:21 +0000

    Remove old stuff

--- src/db.js
+++
@@ -1,7 +0,0 @@
-import mongoose from 'mongoose';
-
-mongoose.connect('mongodb://localhost:27017/library-management');
-const db = mongoose.connection;
-db.on('error', console.error.bind(console, 'MongoDB connection error:'));
-
-export default db;
