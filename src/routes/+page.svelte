c2b39bdc110db4e31e67098be58486d3045551ae
Ajaykmr093 <100403386+Ajaykmr093@users.noreply.github.com>
2024-03-21 03:32:11 +0000

    Update landing page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,12 +1,15 @@
 <script>
 	import logo from '$lib/assets/logo.png';
+	import books from '$lib/assets/books.png';
+	import devices from '$lib/assets/devices.png';
+	import reader from '$lib/assets/reader.png';
 </script>
 
-<div class="w-full h-full flex justify-center items-center">
-	<div class="text-center space-y-4 m-4">
+<header class="w-full h-80v flex justify-center items-center bg-surface-700">
+	<div class="text-center m-4">
 		<img src={logo} alt="Logo" class="mx-auto mb-4 w-32 h-32 filter drop-shadow-lg" />
-		<h1 class="text-2xl font-semibold mb-4">Carry Your Stories Everywhere with eFolio</h1>
-		<div class="flex flex-row gap-4 justify-center">
+		<h3 class="h3 font-semibold mt-4">Carry Your Stories Everywhere with eFolio</h3>
+		<div class="flex flex-row gap-4 justify-center mt-8">
 			<a href="/auth/signup" class="btn variant-filled w-full">
 				<i class="fa-solid fa-user-plus" />
 				<span>Create Account</span>
@@ -17,4 +20,37 @@
 			</a>
 		</div>
 	</div>
+</header>
+<h2 class="h2 text-center font-bold m-8">Explore what eFolio can do</h2>
+
+<div class="flex flex-col lg:flex-row justify-center items-center lg:items-stretch gap-6 mx-12">
+	<div class="card overflow-hidden max-w-lg">
+		<header>
+			<img src={books} alt="Books" />
+		</header>
+		<section class="p-4 space-y-4">
+			<h5 class="h5 font-semibold">Millions of titles at your fingertips</h5>
+			<p>Browse books, magazines, and comics, and sample them before you buy.</p>
+		</section>
+	</div>
+	<div class="card overflow-hidden max-w-lg">
+		<header>
+			<img src={devices} alt="Devices" />
+		</header>
+		<section class="p-4 space-y-4">
+			<h5 class="h5 font-semibold">Read on all your devices</h5>
+			<p>Sign in with your account to access your books and other content on any device.</p>
+		</section>
+	</div>
+	<div class="card overflow-hidden max-w-lg">
+		<header>
+			<img src={reader} alt="Reader" />
+		</header>
+		<section class="p-4 space-y-4">
+			<h5 class="h5 font-semibold">Designed for every reader</h5>
+			<p>Customize your eFolio experience to fit your reading needs and preferences.</p>
+		</section>
+	</div>
 </div>
+
+<div class="mb-12"></div>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,14 +1,30 @@
-import { join } from 'path'
-import type { Config } from 'tailwindcss'
+import { join } from 'path';
+import type { Config } from 'tailwindcss';
 import forms from '@tailwindcss/forms';
 import typography from '@tailwindcss/typography';
-import { skeleton } from '@skeletonlabs/tw-plugin'
+import { skeleton } from '@skeletonlabs/tw-plugin';
 
 export default {
 	darkMode: 'class',
-	content: ['./src/**/*.{html,js,svelte,ts}', join(require.resolve('@skeletonlabs/skeleton'), '../**/*.{html,js,svelte,ts}')],
+	content: [
+		'./src/**/*.{html,js,svelte,ts}',
+		join(require.resolve('@skeletonlabs/skeleton'), '../**/*.{html,js,svelte,ts}')
+	],
 	theme: {
-		extend: {},
+		extend: {
+			height: {
+				'10v': '10vh',
+				'20v': '20vh',
+				'30v': '30vh',
+				'40v': '40vh',
+				'50v': '50vh',
+				'60v': '60vh',
+				'70v': '70vh',
+				'80v': '80vh',
+				'90v': '90vh',
+				'100v': '100vh'
+			}
+		}
 	},
 	plugins: [
 		forms,
@@ -18,10 +34,10 @@ export default {
 				preset: [
 					{
 						name: 'crimson',
-						enhancements: true,
-					},
-				],
-			},
-		}),
-	],
+						enhancements: true
+					}
+				]
+			}
+		})
+	]
 } satisfies Config;
