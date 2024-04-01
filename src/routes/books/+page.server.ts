6dff576ff2b8c3fb6ef84cfd6c4aa349562a3516
Vipul Tyagi <90324465+VipulOG@users.noreply.github.com>
2024-04-01 13:33:42 +0000

    Add recently added books section

--- src/routes/books/+page.server.ts
+++ src/routes/books/+page.server.ts
@@ -1,6 +1,29 @@
+import type { Book } from '$lib/schema/book';
+import { db } from '$lib/server/surreal';
 import type { PageServerLoad } from './$types';
 import { popularBooks, trendingBooks, mostSoldBooks, booksByFavouriteAuthor } from './data';
 
 export const load = (async () => {
-  return { popularBooks, trendingBooks, mostSoldBooks, booksByFavouriteAuthor };
+  let recentBooks: Book[] = [];
+  try {
+    const st = `
+      {
+        let $a = select posted_at, out as book from sell;
+        let $b = select * from $a order by posted_at desc;
+        return $b.book[*];
+      }
+    `;
+    const res = await db.query<[Book[]]>(st);
+    recentBooks = res[0];
+  } catch (err) {
+    console.error(err);
+    console.log('Failed to query books');
+  }
+  return {
+    recentBooks,
+    popularBooks,
+    trendingBooks,
+    mostSoldBooks,
+    booksByFavouriteAuthor
+  };
 }) satisfies PageServerLoad;
--- src/routes/books/+page.svelte
+++ src/routes/books/+page.svelte
@@ -3,12 +3,40 @@
   import HorizontalScroll from '$lib/components/HorizontalScroll.svelte';
   import type { Book } from '$lib/schema/book';
 
+  $: recentBooks = $page.data.recentBooks as Book[];
   $: popularBooks = $page.data.popularBooks as Book[];
   $: trendingBooks = $page.data.trendingBooks as Book[];
   $: mostSoldBooks = $page.data.mostSoldBooks as Book[];
   $: booksByFavouriteAuthor = $page.data.booksByFavouriteAuthor as Book[];
 </script>
 
+<div class="mt-12"></div>
+<div>
+  <div class="px-5 xl:px-14">
+    <h4 class="cursor-pointer text-lg font-bold xl:text-[20px]">Recently added Books</h4>
+  </div>
+  <HorizontalScroll>
+    {#each recentBooks as book (book.id)}
+      <a
+        href="/"
+        class="flex w-[135px] shrink-0 flex-col gap-3 duration-300 ease-out hover:-translate-y-1 hover:brightness-125 xl:w-[185px]"
+      >
+        <div class="h-[190px] overflow-hidden rounded-lg xl:h-[265px]">
+          <img
+            class="object-cover brightness-90"
+            src={book.cover?.medium}
+            alt="cover"
+            title={book.title}
+            loading="lazy"
+          />
+        </div>
+        <p class="line-clamp-2 text-[15px] xl:text-base">
+          {book.title}
+        </p>
+      </a>
+    {/each}
+  </HorizontalScroll>
+</div>
 <div class="mt-12"></div>
 <div>
   <div class="px-5 xl:px-14">
@@ -36,7 +64,6 @@
     {/each}
   </HorizontalScroll>
 </div>
-<div class="mt-12"></div>
 <div class="mt-12"></div>
 <div>
   <div class="px-5 xl:px-14">
