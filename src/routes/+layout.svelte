76e4ae7c588f2f047ec7c6f05890292354f9aecf
Ajay Kumar <100403386+Ajaykmr093@users.noreply.github.com>
2024-04-05 16:43:10 +0000

    Remove unwanted comments

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,45 +1,32 @@
 <script lang="ts">
-  // Global Stylesheets
   import '../app.postcss';
 
-  // Font Awesome
   import '@fortawesome/fontawesome-free/css/fontawesome.css';
   import '@fortawesome/fontawesome-free/css/brands.css';
   import '@fortawesome/fontawesome-free/css/solid.css';
 
-  // Components & Utilities
   import { AppShell, initializeStores, Toast } from '@skeletonlabs/skeleton';
   initializeStores();
 
-  // Dependency: Floating UI
   import { computePosition, autoUpdate, flip, shift, offset, arrow } from '@floating-ui/dom';
   import { storePopup } from '@skeletonlabs/skeleton';
   storePopup.set({ computePosition, autoUpdate, flip, shift, offset, arrow });
 
-  // SvelteKit Imports
   import { page } from '$app/stores';
 
-  // Local Components
   import Appbar from '$lib/components/MainAppbar.svelte';
   import Footer from '$lib/components/MainFooter.svelte';
 
   $: user = $page.data.user;
 </script>
 
-<!-- Overlays -->
 <Toast />
 
-<!-- App Shell -->
 <AppShell>
-  <!-- Header -->
   <svelte:fragment slot="header">
     <Appbar {user} />
   </svelte:fragment>
-
-  <!-- Page Content -->
   <slot />
-
-  <!-- Page Footer -->
   <svelte:fragment slot="pageFooter">
     <Footer />
   </svelte:fragment>
