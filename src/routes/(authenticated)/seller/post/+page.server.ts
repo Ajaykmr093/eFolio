50f8c378870cc9366ce697a3413666c249ad011c
Ajay Kumar <100403386+Ajaykmr093@users.noreply.github.com>
2024-04-05 15:49:14 +0000

    Update book post query

--- src/routes/(authenticated)/seller/post/+page.server.ts
+++ src/routes/(authenticated)/seller/post/+page.server.ts
@@ -1,7 +1,7 @@
 import { zod } from 'sveltekit-superforms/adapters';
 import type { PageServerLoad } from './$types';
 import { SellerBookPostSchema } from './schema';
-import { fail, superValidate, withFiles } from 'sveltekit-superforms';
+import { fail, message, superValidate } from 'sveltekit-superforms';
 import { db } from '$lib/surreal';
 import type { Book } from '$lib/schema/book';
 import { unlink } from 'fs/promises';
@@ -14,7 +14,7 @@ export const load = (async () => {
 }) satisfies PageServerLoad;
 
 export const actions = {
-  default: async ({ request, locals }) => {
+  default: async ({ request }) => {
     const form = await superValidate(request, zod(SellerBookPostSchema));
 
     if (!form.valid) {
@@ -36,41 +36,44 @@ export const actions = {
       cover_url: coverPath
     };
 
+    const sell = {
+      price: form.data.price,
+      discount: form.data.discount,
+      book_url: bookPath
+    };
+
     try {
       const st = `
         {
-          LET $bookRecord = CREATE ONLY book CONTENT {
-            title: $book.title,
-            description: $book.description,
-            publish_date: $book.publishDate,
-            cover: $book.cover
+          let $book = create only book content {
+            title: $bookInfo.title,
+            description: $bookInfo.description,
+            publish_date: $bookInfo.publish_date,
+            cover_url: $bookInfo.cover_url
           };
-          relate $seller->sell->$bookRecord CONTENT {
-            price: $price,
-            discount: $discount,
-            book_url: $bookUrl,
-            in: $seller,
-            out: $bookRecord.id
+
+          let $bookRecord = $book.id;
+          let $sellerRecord = $auth.seller_id;
+
+          let $sellRecord = relate only $sellerRecord->sell->$bookRecord CONTENT {
+              price: $sellInfo.price,
+              discount: $sellInfo.discount,
+              book_url: $sellInfo.book_url,
+              in: $sellerRecord,
+              out: $bookRecord
           };
-          return true;
+
+          return ture;
         };
-        `;
-
-      const vars = {
-        book,
-        seller: locals.user?.seller_id,
-        price: form.data.price,
-        discount: form.data.discount / 100,
-        bookUrl: bookPath
-      };
-
+      `;
+      const vars = { bookInfo: book, sellInfo: sell };
       await db.query<[boolean]>(st, vars);
     } catch (err) {
       console.error(err);
-      console.log('Failed to create post.');
+      console.log('Failed to post book.');
       await unlink(coverPath);
       await unlink(bookPath);
-      return withFiles({ form });
+      return message(form, 'Somthing went wrong.', { status: 500 });
     }
 
     return redirect(302, '/seller');
