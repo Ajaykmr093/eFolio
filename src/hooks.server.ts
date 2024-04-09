3835edc2ee13e60176f82da59e4a2fb4d74ce891
Ajay Kumar <100403386+Ajaykmr093@users.noreply.github.com>
2024-04-09 16:14:12 +0000

    Fix seller portal auth redirects

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -33,7 +33,7 @@ const auth = (async ({ event, resolve }) => {
       const user = (await db.info().catch(async (err) => {
         console.error(err);
         console.log('Failed to get uesr info from token.');
-        if (secureRoute) return await logout(locals, cookies);
+        if (secureRoute) await logout(locals, cookies);
       })) as User;
       event.locals.user = user;
     }
@@ -42,15 +42,24 @@ const auth = (async ({ event, resolve }) => {
   }
 
   const sellerRoute = event.url.pathname.includes('/seller');
-  const sellerApplicationRoute = event.url.pathname.includes('/seller/apply');
-  const isSeller = event.locals.user?.seller_id != undefined;
+  const sellerApplicationRoute = event.url.pathname.includes('/seller/application');
+  const isSeller = event.locals.user?.seller_profile != undefined;
 
-  if (sellerRoute && !sellerApplicationRoute && !isSeller) {
-    return redirect(303, '/seller/apply');
+  let isVerified = false;
+  try {
+    const st = `
+      $res = SELECT application_status FROM ONLY applies_to_become WHERE in = $uid LIMIT 1;
+      $res['application_status'] == 'approved';
+    `;
+    const result = await db.query<[null, boolean]>(st, { uid: event.locals.user!.id });
+    isVerified = result[1];
+  } catch (error) {
+    console.error(error);
+    console.log('Failed to verify seller status.');
   }
 
-  if (sellerApplicationRoute && isSeller) {
-    return redirect(303, '/seller');
+  if (sellerRoute && !sellerApplicationRoute && (!isSeller || !isVerified)) {
+    return redirect(303, '/seller/application');
   }
 
   return await resolve(event);
