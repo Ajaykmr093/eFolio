810c9a4806333510688ec35625f84a29742294d6
Ajay Kumar <100403386+Ajaykmr093@users.noreply.github.com>
2024-04-23 16:53:53 +0000

    Optimize query

--- src/routes/book/[id]/+page.server.ts
+++ src/routes/book/[id]/+page.server.ts
@@ -7,21 +7,14 @@ export const load = (async ({ params }) => {
     SELECT price, discount, out.title AS title, out.coverUrl AS coverUrl, out.publishDate as publishDate,
      out.description as description, out.language as language, out.bookType as bookType, out.totalPages as totalPages,
      out.publication as publication, out.author.name as author.name, out.author.id as author.id, meta::id(out.id) AS id
-    FROM only (SELECT value <-sells[0] FROM only $bookId);
+    FROM only (SELECT VALUE <-sells[0] FROM only $bookId);
+    
+    SELECT price, discount, out.title AS title, out.coverUrl AS coverUrl, meta::id(out.id) AS id
+    FROM (SELECT VALUE <-sells[0] FROM (SELECT id FROM book WHERE author = $authorId AND id != $bookId LIMIT 10));
   `;
   const bookId = `book:${params.id}`;
-  const result = await db.query<[Book]>(st, { bookId });
+  const result = await db.query<[Book, Book[]]>(st, { bookId });
   const book = result[0];
-
-  const st2 = `
-    select price, discount, out.title AS title, out.coverUrl AS coverUrl, meta::id(out.id) AS id
-    from (select value <-sells[0] from (SELECT id FROM book WHERE author = $authorId and id != $bookId limit 10));
-  `;
-  const result2 = await db.query<[Book[]]>(st2, { authorId: book.author.id, bookId });
-  const moreByAuthor = result2[0];
-
-  // const st3 = `throw 'not implemented';`;
-  // const result3 = await db.query<[Book[]]>(st3, { bookId });
-  // const recommendation = result3[0];
+  const moreByAuthor = result[1];
   return { book, moreByAuthor };
 }) satisfies PageServerLoad;
