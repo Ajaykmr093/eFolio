c632d3bd6c704754f9041e7e6cd4772face6878b
Ajay Kumar <100403386+Ajaykmr093@users.noreply.github.com>
2024-04-24 13:53:03 +0000

    Added explore page

---
+++ src/routes/books/+page.server.ts
@@ -0,0 +1,37 @@
+import type { Book } from '$lib/schema/Book';
+import { db } from '$lib/server/db/surreal';
+import type { PageServerLoad } from './$types';
+
+export const load = (async () => {
+  const st = `
+      SELECT * FROM book ORDER BY createdAt DESC LIMIT 20;
+      SELECT * FROM book ORDER BY RAND() LIMIT 20;
+      SELECT * FROM book ORDER BY publishDate DESC LIMIT 20;
+      SELECT * FROM book ORDER BY publishDate ASC LIMIT 20;
+      $randAuthor = SELECT id, name FROM ONLY author ORDER BY RAND() LIMIT 1;
+      RETURN $randAuthor.name;
+      SELECT * FROM book WHERE author = $randAuthor.id;
+    `;
+  const result = await db.query<[Book[], Book[], Book[], Book[], null, string, Book[]]>(st);
+  const [
+    newlyAddedBooks,
+    randomlySelectedBooks,
+    newlyPublishedBooks,
+    oldBooks,
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    _,
+    randomAuthorName,
+    booksByRandomAuthor
+  ] = result;
+
+  console.log(result);
+
+  return {
+    newlyAddedBooks,
+    randomlySelectedBooks,
+    newlyPublishedBooks,
+    oldBooks,
+    randomAuthorName,
+    booksByRandomAuthor
+  };
+}) satisfies PageServerLoad;
---
+++ src/routes/books/+page.svelte
@@ -0,0 +1,77 @@
+<script lang="ts">
+  import { goto } from '$app/navigation';
+  import HorizontalScroll from '$lib/components/HorizontalScroll.svelte';
+  import MediaItem from '$lib/components/MediaItem.svelte';
+  import type { PageData } from './$types';
+
+  export let data: PageData;
+
+  $: newlyAddedBooks = data.newlyAddedBooks;
+  $: randomlySelectedBooks = data.randomlySelectedBooks;
+  $: newlyPublishedBooks = data.newlyPublishedBooks;
+  $: oldBooks = data.oldBooks;
+  $: randomAuthorName = data.randomAuthorName;
+  $: booksByRandomAuthor = data.booksByRandomAuthor;
+</script>
+
+<div class="m-auto mx-6 my-16 max-w-screen-xl justify-between xl:mx-auto">
+  <div class="mt-16">
+    <div class="flex justify-between">
+      <h4 class="h4 font-bold">Recently Added</h4>
+      <p><i class="fa-solid fa-chevron-right"></i></p>
+    </div>
+    <HorizontalScroll>
+      {#each newlyAddedBooks as item}
+        <MediaItem {...item} cover={item.coverUrl} on:click={() => goto(`/book/${item.id}`)} />
+      {/each}
+    </HorizontalScroll>
+  </div>
+
+  <div class="mt-16">
+    <div class="flex justify-between">
+      <h4 class="h4 font-bold">Randoms</h4>
+      <p><i class="fa-solid fa-chevron-right"></i></p>
+    </div>
+    <HorizontalScroll>
+      {#each randomlySelectedBooks as item}
+        <MediaItem {...item} cover={item.coverUrl} on:click={() => goto(`/book/${item.id}`)} />
+      {/each}
+    </HorizontalScroll>
+  </div>
+
+  <div class="mt-16">
+    <div class="flex justify-between">
+      <h4 class="h4 font-bold">Recently published</h4>
+      <p><i class="fa-solid fa-chevron-right"></i></p>
+    </div>
+    <HorizontalScroll>
+      {#each newlyPublishedBooks as item}
+        <MediaItem {...item} cover={item.coverUrl} on:click={() => goto(`/book/${item.id}`)} />
+      {/each}
+    </HorizontalScroll>
+  </div>
+
+  <div class="mt-16">
+    <div class="flex justify-between">
+      <h4 class="h4 font-bold">Books by {randomAuthorName}</h4>
+      <p><i class="fa-solid fa-chevron-right"></i></p>
+    </div>
+    <HorizontalScroll>
+      {#each booksByRandomAuthor as item}
+        <MediaItem {...item} cover={item.coverUrl} on:click={() => goto(`/book/${item.id}`)} />
+      {/each}
+    </HorizontalScroll>
+  </div>
+
+  <div class="mt-16">
+    <div class="flex justify-between">
+      <h4 class="h4 font-bold">Classics</h4>
+      <p><i class="fa-solid fa-chevron-right"></i></p>
+    </div>
+    <HorizontalScroll>
+      {#each oldBooks as item}
+        <MediaItem {...item} cover={item.coverUrl} on:click={() => goto(`/book/${item.id}`)} />
+      {/each}
+    </HorizontalScroll>
+  </div>
+</div>
